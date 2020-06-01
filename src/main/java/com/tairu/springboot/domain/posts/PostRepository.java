4cb5dc50192e651499aba19a4125380e51ce7438
dongwkim <dotorry@gmail.com>
2020-06-01 22:09:23 +0900

    ch3.2

---
+++ src/main/java/com/tairu/springboot/domain/posts/PostRepository.java
@@ -0,0 +1,7 @@
+package com.tairu.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+
+}
